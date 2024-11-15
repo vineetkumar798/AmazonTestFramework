b145cdf8610c482f6117d3c438c939943040b3ce
vineetkumar798 <159282208+vineetkumar798@users.noreply.github.com>
2024-11-16 01:05:41 +0530

    Update Hooks.java

--- src/test/java/com/nagarro/TestRunner/Hooks.java
+++ src/test/java/com/nagarro/TestRunner/Hooks.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Hooks {
     public static WebDriver driver;
